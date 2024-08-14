f802b1c5051901f1d89cdce497d371be8ff2c92f
CaffeineAddiction <c@ffeineaddiction.com>
2024-08-14 00:49:51 +0000

    adding debugging

--- tSCP.lua
+++ tSCP.lua
@@ -1,92 +1,77 @@
--- tSCP.lua
+-- tSSHd.lua
 
 local lib_ssh = require("lib_ssh")
-
-local args = {...}
-if #args < 2 then
-    print("Usage: tSCP <src> <dest>")
-    print("Example: tSCP foo.lua 1: or tSCP foo.lua 1:bar.lua")
-    return
-end
 
 if not lib_ssh.setupModem() then
     print("Failed to setup modem")
     return
 end
 
-local function parseAddress(addr)
-    local id, path = addr:match("(%d+):(.*)")
-    if id then
-        return tonumber(id), path
-    else
-        return nil, addr
-    end
+print("SSH daemon started. Waiting for connections...")
+
+local function sendError(sender, message)
+    print("Sending error to " .. sender .. ": " .. message)
+    lib_ssh.sendMessage(sender, {type="error", message=message})
 end
 
-local function getFilename(path)
-    return path:match("([^/\\]+)$")
-end
-
-local srcId, srcPath = parseAddress(args[1])
-local destId, destPath = parseAddress(args[2])
-
--- If destPath is empty (ends with ':'), use the source filename
-if destPath == "" then
-    destPath = getFilename(srcPath)
-end
-
-local function readFile(path)
-    local file = fs.open(path, "r")
-    if not file then
-        return nil, "Unable to open file: " .. path
-    end
-    local content = file.readAll()
-    file.close()
-    return content
-end
-
-local function writeFile(path, content)
-    local file = fs.open(path, "w")
-    if not file then
-        return false, "Unable to create file: " .. path
-    end
-    file.write(content)
-    file.close()
-    return true
-end
-
-if srcId then
-    -- Remote to local
-    lib_ssh.sendMessage(srcId, {type="read_file", path=srcPath})
-    local sender, response = lib_ssh.receiveMessage(5)
-    if sender == srcId and response and response.type == "file_content" then
-        local success, err = writeFile(destPath, response.content)
-        if success then
-            print("File transferred successfully")
+while true do
+    local sender, message = lib_ssh.receiveMessage()
+    if sender and message then
+        print("Received message from " .. sender .. ": " .. textutils.serialize(message))
+        
+        if message.type == "execute" then
+            local func, err = load(message.command)
+            if func then
+                local ok, result = pcall(func)
+                if ok then
+                    print("Sending execution result")
+                    lib_ssh.sendMessage(sender, {type="result", output=tostring(result)})
+                else
+                    sendError(sender, "Execution error: " .. tostring(result))
+                end
+            else
+                sendError(sender, "Compilation error: " .. tostring(err))
+            end
+        elseif message.type == "ls" then
+            local files = fs.list(".")
+            print("Sending ls result")
+            lib_ssh.sendMessage(sender, {type="ls_result", files=files})
+        elseif message.type == "cd" then
+            if fs.isDir(message.dir) then
+                shell.setDir(message.dir)
+                print("Changed directory, sending confirmation")
+                lib_ssh.sendMessage(sender, {type="cd_result", message="Changed to " .. message.dir})
+            else
+                sendError(sender, "Directory not found: " .. message.dir)
+            end
+        elseif message.type == "read_file" then
+            print("Attempting to read file: " .. message.path)
+            if fs.exists(message.path) and not fs.isDir(message.path) then
+                local file = fs.open(message.path, "r")
+                if file then
+                    local content = file.readAll()
+                    file.close()
+                    print("File read successfully, sending content")
+                    lib_ssh.sendMessage(sender, {type="file_content", content=content})
+                else
+                    sendError(sender, "Unable to open file: " .. message.path)
+                end
+            else
+                sendError(sender, "File not found: " .. message.path)
+            end
+        elseif message.type == "write_file" then
+            print("Attempting to write file: " .. message.path)
+            local file = fs.open(message.path, "w")
+            if file then
+                file.write(message.content)
+                file.close()
+                print("File written successfully, sending confirmation")
+                lib_ssh.sendMessage(sender, {type="file_written"})
+            else
+                sendError(sender, "Unable to create file: " .. message.path)
+            end
         else
-            print("Error writing file: " .. err)
+            sendError(sender, "Unknown command type: " .. message.type)
         end
-    elseif sender == srcId and response and response.type == "error" then
-        print("Error reading remote file: " .. response.message)
-    else
-        print("Error: No response or unexpected response from remote")
     end
-elseif destId then
-    -- Local to remote
-    local content, err = readFile(srcPath)
-    if content then
-        lib_ssh.sendMessage(destId, {type="write_file", path=destPath, content=content})
-        local sender, response = lib_ssh.receiveMessage(5)
-        if sender == destId and response and response.type == "file_written" then
-            print("File transferred successfully")
-        elseif sender == destId and response and response.type == "error" then
-            print("Error writing remote file: " .. response.message)
-        else
-            print("Error: No response or unexpected response from remote")
-        end
-    else
-        print("Error reading local file: " .. err)
-    end
-else
-    print("Invalid source or destination")
 end
