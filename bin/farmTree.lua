b04b0bc5b9734dfab740d23b5dcfc17423ed317d
lordy <Lordy2001@gmail.com>
2024-08-25 01:09:42 +0000

    added item types

--- bin/farmTree.lua
+++ bin/farmTree.lua
@@ -1,9 +1,52 @@
 m = require("/lib/move")
 f = require("/lib/farming")
+--       0       
+--
+-- 2   c l c     
+-- 1    lsl     
+-- 0   lsTsl     
+-- -1   lsl     
+-- -2  c l c      
 
-m.setHome()
-f.waitForTree()
-print("Tree found Mining!")
-f.mineTree()
-print("Going Home")
-m.goHome()
+--      0
+-- 2
+-- 1    f
+-- 0    T
+--      l
+-- 
+
+
+while true do
+    m.setHome()
+    ::continue::
+    f.waitForTree()
+    print("Tree found Mining!")
+    f.mineTree()
+    print("Get the leaves")
+    f.mineLeaves()
+    print("Going Home")
+    m.goTo(0,0,2,1,0) --above the furnace
+    f.fillFurnace("Down")
+    if f.isTree() then
+        goto continue
+    end
+    m.goForward(true)
+    m.goDown(true)
+    m.turnRight()
+    m.turnRight()
+    f.fillFurnace("Forward")
+    m.goDown(true)
+    m.goForward(true)
+    turtle.suckUp()
+    m.goto(2,2,0,1,0)
+    f.dumpWood()
+    m.goto(2,-2,0,1,0)
+    f.dumpCharcoal()
+    m.goto(-2,2,0,1,0)
+    f.dumpOther()
+    
+    print("Sweep up")
+    f.sweepUp()
+    m.goHome()
+    sleep(0.5)
+end
--- lib/farming.lua
+++ lib/farming.lua
@@ -12,12 +12,19 @@ function farm.waitForTree()
     end
 end
 
+function farm.isTree()
+    return itemTypes.isTreeFwd()
+end
+
 function farm.mineTree()
     --move into the trunk
     move.goForward(true)
     turtle.digDown()
     sleep(0.5)
     turtle.suckDown()
+    if itemTypes.selectSapling() then
+        turtle.placeDown()
+    end
     local blockUp, info = turtle.inspectUp()
     while itemTypes.isTreeUp() do
         move.goUp(true)
@@ -27,5 +34,40 @@ end
 
 function farm.mineLeaves()
 end
+
+function farm.sweepUp(radius)
+end
+
+function farm.fillFurnace(dir)
+    woodCount = itemTypes.getWood()
+    if woodCount then
+        if dir = "Down" then 
+            turtle.dropDown(woodCount/3)
+        elseif dir = "Forward" then
+            turtle.drop(woodCount/3)
+        end
+    end
+end
+
+function farm.dumpWood()
+    while itemTypes.getWood() do
+        turtle.dropDown()
+    end
+end
+
+function farm.dumpCharcoal()
+    itemTypes.selectItem("minecraft:charcoal")
+    move.refuel()
+    while itemTypes.selectItem("minecraft:charcoal") do
+        turtle.dropDown()
+    end
+end
+
+function farm.dumpOther()
+    for slot = 1
+end
+
+
+
 
 return farm
