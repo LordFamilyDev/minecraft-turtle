d3c15e1cd107ba7cb5eb5913a87a3bd1f09602dd
Tom Lord <thomasjacoblord@gmail.com>
2024-08-29 15:56:16 +0000

    bugfixes

--- bin/TL_Random.lua
+++ bin/TL_Random.lua
@@ -3,53 +3,10 @@ local lib_itemTypes = require("/lib/item_types")
 local lib_move = require("/lib/move")
 local lib_farming = require("/lib/farming")
 
-function transferMaterial(source, destination, blockName, amount)
-    -- Wrap the source and destination chests
-    --local source = peripheral.wrap(fromChest)
-    --local destination = peripheral.wrap(toChest)
-
-    -- Iterate through all slots in the source chest
-    for slot, item in pairs(source.list()) do
-        -- Check if the item is stone
-        if item.name == blockName then
-            -- Attempt to transfer the stone to the destination chest
-            local transferred = source.pushItems(peripheral.getName(destination), slot,amount)
-            return transferred
-        end
-    end
-    return 0
-end
-
-function requestMaterial(blockName, amount, userChest, storageChests)
-    local transferredCount = 0
-    for i = 1, #storageChests do
-        local transferred = transferMaterial(storageChests[i],userChest,blockName,amount)
-        transferredCount = transferredCount - transferred
-        if transferredCount >= amount then
-            return true
-        end
-    end
-    return false
-end
-
-function clearUserStorage(userChest, storageChests)
-    -- Iterate through all slots in the user chest
-    for slot, item in pairs(userChest.list()) do
-        -- Try to move the item to one of the storage chests
-        for _, storageChest in ipairs(storageChests) do
-            local transferred = userChest.pushItems(peripheral.getName(storageChest), slot)
-            
-            -- If all items from the slot were transferred, move to the next slot
-            if transferred == item.count then
-                break
-            else
-                -- If some items remain, update the item count and try the next chest
-                item.count = item.count - transferred
-            end
-        end
-    end
-
-    print("User chest cleared.")
+function toFile(string)
+    local file = fs.open("debugFile", "w")
+    file.writeLine(string)
+    file.close()
 end
 
 function inspectDownToPrint()
@@ -69,6 +26,144 @@ function inspectDownToPrint()
         end
     else
         print("No block detected.")
+    end
+end
+
+function isTargetBlock(blockInfo, targetBlockNames)
+    --toFile(textutils.serialize(blockInfo))
+    if lib_itemTypes.isItemInList(blockInfo.name, targetBlockNames) then
+        if blockInfo.name == "minecraft:lava" and blockInfo.state.level > 0 then
+            return false
+        else
+            return true
+        end
+    end
+    return false
+end
+
+function floodFill(targetBlockNames)
+
+    lib_move.setHome()
+
+    while true do
+        if lib_move.distToHome() >= 64 then
+            print("out of range, terminating program")
+            lib_move.goHome()
+            return
+        end
+
+        --move in first direction that has a block on target list
+        local success, blockInfo
+        local moveMade = false
+        --up
+        if not moveMade then
+            success, blockInfo = turtle.inspectUp()
+            if success and isTargetBlock(blockInfo,targetBlockNames) then
+                if not lib_move.charMove("U", true, true) then
+                    print("move failed")
+                    lib_move.goHome()
+                    return false
+                end
+                moveMade = true
+            end
+        end
+
+        --down
+        if not moveMade then
+            success, blockInfo = turtle.inspectDown()
+            if success and isTargetBlock(blockInfo,targetBlockNames) then
+                if not lib_move.charMove("D", true, true) then
+                    print("move failed")
+                    lib_move.goHome()
+                    return false
+                end
+                moveMade = true
+            end
+        end
+
+        --forward
+        if not moveMade then
+            success, blockInfo = turtle.inspect()
+            if success and isTargetBlock(blockInfo,targetBlockNames) then
+                if not lib_move.charMove("F", true, true) then
+                    print("move failed")
+                    lib_move.goHome()
+                    return false
+                end
+                moveMade = true
+            end
+        end
+
+        --left 
+        if not moveMade then
+            lib_move.macroMove("L", false, true)
+            success, blockInfo = turtle.inspect()
+            if success and isTargetBlock(blockInfo,targetBlockNames) then
+                lib_move.appendMoveMem("L")
+                if not lib_move.macroMove("F", true, true) then
+                    print("move failed")
+                    lib_move.goHome()
+                    return false
+                end
+                moveMade = true
+            else
+                lib_move.macroMove("R", false, true)
+            end
+        end
+
+        --back
+        if not moveMade then
+            lib_move.macroMove("LL", false, true)
+            success, blockInfo = turtle.inspect()
+            if success and isTargetBlock(blockInfo,targetBlockNames) then
+                lib_move.appendMoveMem("LL")
+                if not lib_move.macroMove("F", true, true) then
+                    print("move failed")
+                    lib_move.goHome()
+                    return false
+                end
+                moveMade = true
+            else
+                lib_move.macroMove("RR", false, true)
+            end
+        end
+
+        --right
+        if not moveMade then
+            lib_move.macroMove("R", false, true)
+            success, blockInfo = turtle.inspect()
+            if success and isTargetBlock(blockInfo,targetBlockNames) then
+                lib_move.appendMoveMem("R")
+                if not lib_move.macroMove("F", true, true) then
+                    print("move failed")
+                    lib_move.goHome()
+                    return false
+                end
+                moveMade = true
+            else
+                lib_move.macroMove("L", false, true)
+            end
+        end
+
+        --if none step back one move in memory and look again
+        if not moveMade then
+            local turnMoveFlag = false
+            while true do
+                local lastMove = lib_move.popBackMoveMem()
+                if lastMove == "R" or lastMove == "L" then
+                    turnMoveFlag = true
+                else
+                    turnMoveFlag = false
+                end
+                if lastMove == nil then
+                    return true
+                end
+                lib_move.charMove(lib_move.revMoveChar(lastMove),false,true)
+                if not turnMoveFlag then
+                    break
+                end
+            end
+        end
     end
 end
 
@@ -95,21 +190,30 @@ if arg1 then
         lib_move.goForward(true)
         inspectDownToPrint()
     elseif arg1 == 5 then
-        local userChest = peripheral.wrap("minecraft:chest_6")
-        local storageChests = {peripheral.wrap("minecraft:chest_7"),
-        peripheral.wrap("minecraft:chest_8"),
-        peripheral.wrap("minecraft:chest_9"),
-        peripheral.wrap("minecraft:chest_10")}
-
-        requestMaterial(args[2], 32, userChest, storageChests)
+        print("flood fill demo")
+        local targetBlocks = {args[2]}
+        floodFill(targetBlocks)
     elseif arg1 == 6 then
-        local userChest = peripheral.wrap("minecraft:chest_6")
-        local storageChests = {peripheral.wrap("minecraft:chest_7"),
-        peripheral.wrap("minecraft:chest_8"),
-        peripheral.wrap("minecraft:chest_9"),
-        peripheral.wrap("minecraft:chest_10")}
-
-        clearUserStorage(userChest, storageChests)
+        turtle.up()
+        turtle.up()
+        turtle.placeUp()
+        for i = 1, 4 do
+            turtle.place()
+            turtle.turnRight()
+        end
+        turtle.down()
+        turtle.placeUp()
+        turtle.digUp()
+        turtle.forward()
+    elseif arg1 == 7 then
+        local dist = tonumber(args[2])
+        if dist == nil then
+            dist = 1
+        end
+        for i = 0, dist do
+            turtle.digUp()
+            turtle.up()
+        end
     end
     
 else
@@ -118,5 +222,7 @@ else
     print("2: vertical move loop test")
     print("3: spiral sweep test")
     print("4: print block below")
-    print("5: print block below")
+    print("5: flood fill clear")
+    print("6: diving bell")
+    print("7: turtle up")
 end
