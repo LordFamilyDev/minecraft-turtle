3ad694dc4ec4e27d0343b04fea8960a366e8bdc8
lordy <Lordy2001@gmail.com>
2024-09-01 21:03:05 +0000

    mtk -f tunnel now works

--- bin/mtk.lua
+++ bin/mtk.lua
@@ -162,7 +162,7 @@ local macro_functions = {
     ml = function() 
         debug_print("Move left") 
         if move then 
-            return move.goLeft(true)
+            return move.goLeft(true,true)
         else 
             return false
         end 
@@ -170,7 +170,7 @@ local macro_functions = {
     mr = function() 
         debug_print("Move right") 
         if move then 
-            return move.goRight(true)
+            return move.goRight(true,true)
         else 
             return false
         end 
@@ -293,25 +293,37 @@ local macro_functions = {
         return turtle.placeDown()
     end,
     Pf = function() 
-        debug_print("Blindly place forward") 
-        if not turtle.place() then
-            return false, "No items to place"
+        debug_print("Force Place forward")
+        while turtle.dig() do end
+        local current_slot = turtle.getSelectedSlot()
+        if turtle.getItemCount(current_slot) <= 1 then
+            if not replenish_slot(current_slot) then
+                return false, "Failed to replenish items for placing"
+            end
         end
-        return true
+        return turtle.place()
     end,
     Pu = function() 
-        debug_print("Blindly place up") 
-        if not turtle.placeUp() then
-            return false, "No items to place"
+        debug_print("Place up")
+        while turtle.digUp() do end
+        local current_slot = turtle.getSelectedSlot()
+        if turtle.getItemCount(current_slot) <= 1 then
+            if not replenish_slot(current_slot) then
+                return false, "Failed to replenish items for placing"
+            end
         end
-        return true
+        return turtle.placeUp()
     end,
     Pd = function() 
-        debug_print("Blindly place down") 
-        if not turtle.placeDown() then
-            return false, "No items to place"
+        debug_print("Place down") 
+        while turtle.digDown() do end
+        local current_slot = turtle.getSelectedSlot()
+        if turtle.getItemCount(current_slot) <= 1 then
+            if not replenish_slot(current_slot) then
+                return false, "Failed to replenish items for placing"
+            end
         end
-        return true
+        return turtle.placeDown()
     end,
     lf = function() 
         debug_print("Look forward")
@@ -487,14 +499,17 @@ end
 -- Command-line interface
 local function print_usage()
     print("Usage: mtk [-m <macro_string>] [-l <loop_count>] [-i <start_index>] [-v] [-t] [-S <save_path>] [-s <load_path>]")
+    io.read()
     print("  -m, --macro    Macro string (required unless -t is used)")
     print("  -l, --loop     Number of times to loop the macro (optional, default: 1)")
     print("  -i, --index    Starting index for the macro (optional, default: 1)")
     print("  -v, --verbose  Enable debug output")
+    io.read()
     print("  -t, --test     Enter test interface (REPL mode)")
     print("  -S <path>      Serialize and save inventory snapshot to file")
     print("  -s <path>      Load inventory snapshot from file")
     print("  -x             Label inner loop lengths in order: [loop count x1] [loop count x2] etc")
+    io.read()
     print("  -h, --help     Print this help message")
 end
 
@@ -531,6 +546,54 @@ local function run_test_interface()
             end
         end
     end
+end
+
+function string.trim(s)
+    return s:match("^%s*(.-)%s*$")
+end
+
+function parseFile(fileName)
+    -- Add .mtk extension if there's no extension
+    if not fileName:match("%.%w+$") then
+        fileName = fileName .. ".mtk"
+    end
+
+    -- Check if the file exists in the current directory
+    debug_print("Checking for file:"..fileName)
+    if not fs.exists(fileName) then
+        -- If not, check in the /mtk/ directory
+        fileName = "/mtk/" .. fileName
+        if not fs.exists(fileName) then
+            error("File not found: " .. fileName)
+        end
+    end
+
+    local file = fs.open(fileName, "r")
+    if not file then
+        error("Could not open file: " .. fileName)
+    end
+
+    local arguments = {}
+    local currentValue = nil
+
+    for line in file.readLine do
+        line = line:trim()
+        if #line > 0 and line:sub(1, 1) ~= "#" then
+            if currentValue then
+                currentValue = currentValue:sub(1, -2) .. line -- Remove the '\' and add the new line
+            else
+                currentValue = line
+            end
+            -- Check if the line ends with '\'
+            if currentValue:sub(-1) ~= "\\" then
+                table.insert(arguments,currentValue:trim())
+                currentValue = nil
+            end
+        end
+    end
+
+    file.close()
+    return arguments
 end
 
 function mtk.run_cli(args)
@@ -575,8 +638,17 @@ function mtk.run_cli(args)
         elseif args[i] == "-s" then
             i = i + 1
             load_path = args[i]
+        elseif args[i] == "-f" then
+            i = i + 1
+            local fileName = args[i]
+            local fileArgs = parseFile(fileName)
+            for j, argPair in ipairs(fileArgs) do
+                debug_print("Adding arg:" .. argPair)
+                table.insert(args, i + j, argPair)
+            end
         else
             print("Unknown argument: " .. args[i])
+            io.read()
             print_usage()
             return
         end
