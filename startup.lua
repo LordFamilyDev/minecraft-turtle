fea9a0ccb0a4ca22b0b07f24319b4e13819a60b6
caffeineaddiction <caffeineaddiction@gmail.com>
2024-09-09 04:07:56 +0000

    fix for update

--- startup.lua
+++ startup.lua
@@ -1,14 +1,20 @@
 local sPath = shell.path()
 sPath = sPath .. ":/bin"
 shell.setPath(sPath)
-shell.run("/bin/update")
 
 -- Startup Remote Desktop Service
 if term.isColor() then
+    -- check for updates
+    shell.run("background", "httpdupate")
     -- this is an advanced computer
     if fs.exists("/usr/bin/vncd") then
         if fs.exists("/bin/util/wrapper.lua") then
-            shell.run("background", "/bin/util/wrapper", "/usr/bin/vncd")
+            -- check if modem exists
+            if peripheral.find("modem") then
+                shell.run("background", "/bin/util/wrapper", "/usr/bin/vncd")
+            end
         end
     end
+else
+    shell.run("/bin/httpupdate")
 end
